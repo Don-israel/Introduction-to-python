4de651d57b34f6493bb32767ca5b1d467c63946d
Don_israel <israelojakorotu@gmail.com>
2024-09-23 19:41:07 +0100

    first py commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome world")
+print("i am starting python")
+print("coding is fun")
+print("python is a new begining of a new language")
+#this is a comment
+#comment are no executed in the code
+#they are used for communicating with other developers
+print("this is a statement")
